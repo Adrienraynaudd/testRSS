70f4f3e2f4152225de4ef51dfe65f1a8dcc8bf97
adrien <adrien48632@gmail.com>
2023-11-17 15:55:24 +0100

    test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,73 +1,138 @@
-﻿using System;
+﻿using Microsoft.SyndicationFeed;
+using Microsoft.SyndicationFeed.Rss;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.ServiceModel.Syndication;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Xml;
+using Ude;
 
 namespace testRSS
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            LoadRss();
-        }
 
-        private void LoadRss()
-        {
-            RssReader rssReader = new RssReader();
-            List<SyndicationItem> items = rssReader.ReadRss("https://www.courrierinternational.com/feed/rubrique/france/rss.xml");
+            
+            string rssUrl = "https://www.lemondeinformatique.fr/flux-rss/thematique/securite/rss.xml";
 
-            rssListBox.ItemsSource = items;
-        }
-    }
+            
+            Encoding encoding = EncodingDetectionHelper.DetectEncoding(rssUrl);
 
-
-    public class RssReader
-    {
-        public List<SyndicationItem> ReadRss(string url)
-        {
-            List<SyndicationItem> items = new List<SyndicationItem>();
-
-            try
+            
+            XmlReaderSettings readerSettings = new XmlReaderSettings
             {
-                using (System.Net.WebClient client = new System.Net.WebClient())
-                {
-                    using (Stream stream = client.OpenRead(url))
-                    {
-                        // Use StreamReader to detect the encoding
-                        using (StreamReader streamReader = new StreamReader(stream, detectEncodingFromByteOrderMarks: true))
-                        {
-                            using (XmlReader reader = XmlReader.Create(streamReader))
-                            {
-                                SyndicationFeed feed = SyndicationFeed.Load(reader);
+                Async = true,
+                
+            };
 
-                                if (feed != null)
-                                {
-                                    foreach (SyndicationItem item in feed.Items)
-                                    {
-                                        items.Add(item);
-                                    }
-                                }
+            
+            XmlReader reader = XmlReader.Create(rssUrl, readerSettings);
+
+            
+            DisplayRssFeed(rssUrl, reader);
+        }
+
+        
+        private async void DisplayRssFeed(string rssUrl, XmlReader xmlReader)
+        {
+            
+            var rssItems = await RssReadFeed.CreateRssFeedReaderExample(rssUrl, xmlReader);
+
+            
+            rssListBox.ItemsSource = rssItems;
+        }
+
+        public class EncodingDetectionHelper
+        {
+            public static Encoding DetectEncoding(string filePath)
+            {
+                try
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                       
+                        HttpResponseMessage response = client.GetAsync(filePath).Result;
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            Stream stream = response.Content.ReadAsStreamAsync().Result;
+
+                            var detector = new Ude.CharsetDetector();
+                            detector.Feed(stream);
+                            detector.DataEnd();
+
+                            if (detector.Charset != null)
+                            {
+                                return Encoding.GetEncoding("iso-8859-15");
                             }
+                        }
+                        else
+                        {
+                            MessageBox.Show($"La requête a échoué avec le code : {response.StatusCode}");
                         }
                     }
                 }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Une exception s'est produite : {ex.Message}");
+                    
+                }
 
-            return items;
+                
+                return Encoding.UTF8;
+
+            }
         }
 
+
+    }
+    class RssReadFeed
+    {
+        public class ItemClass
+        {
+            public string? Title { get; set; }
+            public string? Description { get; set; }
+        }
+
+        public static async Task<List<ItemClass>> CreateRssFeedReaderExample(string filePath, XmlReader xmlReader)
+        {
+            List<ItemClass> rssItems = new List<ItemClass>();
+
+            var feedReader = new RssFeedReader(xmlReader);
+
+            while (await feedReader.Read())
+            {
+                switch (feedReader.ElementType)
+                {
+                    case SyndicationElementType.Item:
+                        ISyndicationItem item = await feedReader.ReadItem();
+                        ItemClass rssItem = ConvertToYourRssItem(item);
+                        rssItems.Add(rssItem);
+                        break;
+
+                }
+            }
+
+            return rssItems;
+        }
+
+
+
+        private static ItemClass ConvertToYourRssItem(ISyndicationItem syndicationItem)
+        {
+            return new ItemClass
+            {
+                Title = syndicationItem.Title,
+                Description = syndicationItem.Description,
+                
+            };
+        }
 
     }
 }
